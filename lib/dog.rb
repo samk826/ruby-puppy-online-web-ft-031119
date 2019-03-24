2fbde404d91bb53fc2b61d417e27a1b1495abae6
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-24 16:36:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,15 @@
-class Tracker
+class Dog
   attr_accessor :name
   @@all = []
 
-end
+
 
 def initialize
   @name=name
   @@all<<self
+end
+def self.all
+  @@all.each do dog
+    puts dog.name
+  end
 end
